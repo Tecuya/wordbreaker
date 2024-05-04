9173de839be753555010f702655931e89db90327
Sean McLean <sean.mclean@reddit.com>
2024-05-04 16:55:47 -0700

    word game

---
+++ game.js
@@ -0,0 +1,61 @@
+import { dictionary } from './dictionary.js';
+
+const gameBoard = document.getElementById("gameBoard");
+
+let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+const width = 3;
+const height = 3;
+const gameGrid = [];
+
+function findDictEntriesWithPrefix(prefix) {
+  return dictionary.filter(dictEntry => dictEntry[0].toLowerCase().startsWith(prefix.toLowerCase()));
+}
+
+function initializeGrid() {
+  for (let i = 0; i < 3; i++) {
+    let row = [];
+    for (let j = 0; j < 3; j++) {
+        let randomIndex = Math.floor(Math.random() * letters.length);
+        row.push(letters[randomIndex]);
+    }
+    gameGrid.push(row);
+  }
+}
+
+function repeatString(str, n) {
+  return new Array(n).fill(str).join(" ");
+}
+
+const player = {
+  x: 1,
+  y: 1,
+};
+
+const drawBoard = () => {
+
+  const playerCurrentLetter = gameGrid[player.x][player.y];
+
+  gameBoard.style.gridTemplateColumns = repeatString("1fr", gameGrid.length);
+
+  for(var x=0;x<gameGrid.length;x++) {
+    for(var y=0;y<gameGrid[x].length;y++) {
+      const div = document.createElement("div");
+      div.classList.add("item");
+      if(player.x == x && player.y == y) {
+        div.classList.add("currentItem");
+      } else {
+        if(findDictEntriesWithPrefix(playerCurrentLetter + gameGrid[x][y]).length > 0) {
+          div.classList.add("validMove");
+        }
+      }
+      div.innerHTML = gameGrid[x][y];
+
+      gameBoard.appendChild(div);
+    }
+  }
+};
+
+
+initializeGrid();
+drawBoard();
