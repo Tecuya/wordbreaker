2e26084cff996426b9e14f024127736372b372b0
Sean McLean <sean.mclean@reddit.com>
2024-05-04 17:57:22 -0700

    progress

--- game.js
+++ game.js
@@ -2,18 +2,20 @@ import { dictionary } from './dictionary.js';
 
 const gameBoard = document.getElementById("gameBoard");
 const currentLetters = document.getElementById("currentLetters");
+const dictionaryMatches = document.getElementById("dictionaryMatches");
 
 let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 const width = 3;
 const height = 3;
-const gameGrid = [];
+var gameGrid = [];
 
 function findDictEntriesWithPrefix(prefix) {
   return dictionary.filter(dictEntry => dictEntry[0].toLowerCase().startsWith(prefix.toLowerCase()));
 }
 
 function initializeGrid() {
+  gameGrid = [];
   for (let i = 0; i < 3; i++) {
     let row = [];
     for (let j = 0; j < 3; j++) {
@@ -36,6 +38,9 @@ let selectedLetters = ''; // Global string to store selected letters
 
 document.addEventListener('keydown', (event) => {
   switch(event.key) {
+    case 'r':
+      initializeGrid();
+      break;
     case 'ArrowUp':
       cursor.y = Math.max(0, cursor.y - 1);
       break;
@@ -50,19 +55,19 @@ document.addEventListener('keydown', (event) => {
       break;
     case 'Enter':
       selectedLetters += gameGrid[cursor.x][cursor.y];
-      drawBoard();
+      const matchingEntries = findDictEntriesWithPrefix(selectedLetters);
+      dictionaryMatches.innerHTML = JSON.stringify(matchingEntries);
       break;
   }
-  console.log(cursor);
-  drawBoard();
+  draw();
 });
 
-const drawBoard = () => {
+const draw = () => {
 
   gameBoard.innerHTML = '';
   currentLetters.innerHTML = selectedLetters; // Render the selected letters into the "currentLetters" div
 
-  const playerCurrentLetter = gameGrid[cursor.x][cursor.y];
+  const cursorCurrentLetter = gameGrid[cursor.x][cursor.y];
 
   gameBoard.style.gridTemplateColumns = repeatString("1fr", gameGrid.length);
 
@@ -70,15 +75,15 @@ const drawBoard = () => {
     for(var x=0;x<width;x++) {
       const div = document.createElement("div");
       div.classList.add("item");
+
       if(cursor.x == x && cursor.y == y) {
         div.classList.add("cursorItem");
-      } else {
-        const wordSoFar = playerCurrentLetter + gameGrid[x][y];
-        const matchingEntries = findDictEntriesWithPrefix(wordSoFar);
-        if(matchingEntries.length > 0) {
-          console.log("wordsofar",wordSoFar,matchingEntries[0]);
-          div.classList.add("validMove");
-        }
+      }
+
+      const wordSoFar = selectedLetters + gameGrid[x][y];
+      const matchingEntries = findDictEntriesWithPrefix(wordSoFar);
+      if(matchingEntries.length > 0) {
+        div.classList.add("validMove");
       }
       div.innerHTML = gameGrid[x][y];
 
@@ -89,4 +94,4 @@ const drawBoard = () => {
 
 
 initializeGrid();
-drawBoard();
+draw();
